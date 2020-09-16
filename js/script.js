85f421387f93018570d33fdfbd5dd1bdb9c302c5
latikagurnani <gurnanilatika@gmail.com>
2020-09-16 19:54:58 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ $(function(){
             mainClass: 'mfp-with-zoom', // this class is for CSS animation below
 
             zoom: {
-                enabled: true, // By default it's false, so don't forget to enable it
+//                enabled: true, // By default it's false, so don't forget to enable it
 
                 duration: 300, // duration of the effect, in milliseconds
                 easing: 'ease-in-out', // CSS transition easing function
